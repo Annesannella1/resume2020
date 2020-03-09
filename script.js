2fe88f9049b0645797c22748542269a186ae1067
Anne Sannella <annesannella@yahoo.com>
2020-03-09 03:33:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+$(document).ready(() => {
+    $("svg-container-hover").hover(function(){
+       $(this).html("backgroundopacity", "1.0");
+    }, function(){
+       $(this).html("opacity", "0.4");
+    });
+});
