67d86e6ac34ba6b3106bea80db0ae99e8a0e9583
Anne Sannella <annesannella@yahoo.com>
2020-03-10 18:31:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-$(document).ready(() => {
-    $("svg-container-hover").hover(function(){
-       $(this).css("background-image:" "url(./experiencedesignson.png");
-    }, function(){
-       $(this).css("background-image:" "url(./experiencedesigns.svg");
-    });
-});
+/*Anne Sannella 2020*/ 
